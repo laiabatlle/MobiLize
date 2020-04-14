57de5e765621ca0be68be64f12345b566cee5099
ericgs230898 <61179770+ericgs230898@users.noreply.github.com>
2020-04-14 17:13:41 +0200

    Add files via upload

--- Mobilize-App/app/src/main/java/com/app/mobilize/Fragments/ProgresoFragment.java
+++ Mobilize-App/app/src/main/java/com/app/mobilize/Fragments/ProgresoFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +17,9 @@ import com.app.mobilize.R;
  */
 public class ProgresoFragment extends Fragment {
 
+    TextView tvWorkout, tvCycling, tvRunning, tvTotal;
+    ProgressBar pbWorkout, pbCycling, pbRunning;
+
     public ProgresoFragment() {
         // Required empty public constructor
     }
@@ -24,6 +29,31 @@ public class ProgresoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progreso, container, false);
+        View view =  inflater.inflate(R.layout.fragment_progreso, container, false);
+
+        tvWorkout = view.findViewById(R.id.tvWorkout);
+        tvCycling = view.findViewById(R.id.tvCycling);
+        tvRunning = view.findViewById(R.id.tvRunning);
+        tvTotal = view.findViewById(R.id.tvTempsTotal);
+
+        pbWorkout = view.findViewById(R.id.pbWorkout);
+        pbCycling = view.findViewById(R.id.pbCycling);
+        pbRunning = view.findViewById(R.id.pbRunning);
+
+        int minTotals, minRunning, minCycling, minWorkout;
+        minTotals = 2000;
+        minRunning = 1000;
+        minCycling = 500;
+        minWorkout = 500;
+
+        tvWorkout.setText("Workout: " + String.valueOf(minWorkout) + " min");
+        tvRunning.setText("Running: " + String.valueOf(minRunning) + " min");
+        tvCycling.setText("Cycling: " + String.valueOf(minCycling) + " min");
+
+        pbWorkout.setProgress(minWorkout*100/minTotals);
+        pbCycling.setProgress(minCycling*100/minTotals);
+        pbRunning.setProgress(minRunning*100/minTotals);
+
+        return view;
     }
 }
