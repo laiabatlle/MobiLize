d32f7ca89997c9d61999822f8cc245339722171c
AlexBrugal <60064808+AlexBrugal@users.noreply.github.com>
2020-04-14 21:33:06 +0200

    Layout Seguimiento (graph + radiobuttons)

--- Mobilize-App/app/src/main/java/com/app/mobilize/Fragments/ProgresoFragment.java
+++ Mobilize-App/app/src/main/java/com/app/mobilize/Fragments/ProgresoFragment.java
@@ -4,12 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.app.mobilize.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.Map;
 
 
 /**
@@ -19,6 +31,8 @@ public class ProgresoFragment extends Fragment {
 
     TextView tvWorkout, tvCycling, tvRunning, tvTotal;
     ProgressBar pbWorkout, pbCycling, pbRunning;
+    RadioButton rbMensual, rbSemanal, rbPeso, rbCalorias, rbPasos;
+    LineChart chart;
 
     public ProgresoFragment() {
         // Required empty public constructor
@@ -40,6 +54,13 @@ public class ProgresoFragment extends Fragment {
         pbCycling = view.findViewById(R.id.pbCycling);
         pbRunning = view.findViewById(R.id.pbRunning);
 
+        rbSemanal = view.findViewById(R.id.rbSemanal);
+        rbMensual = view.findViewById(R.id.rbMensual);
+
+        rbPeso = view.findViewById(R.id.rbPeso);
+        rbCalorias = view.findViewById(R.id.rbCalorias);
+        rbPasos = view.findViewById(R.id.rbPasos);
+
         int minTotals, minRunning, minCycling, minWorkout;
         minTotals = 2000;
         minRunning = 1000;
@@ -53,6 +74,73 @@ public class ProgresoFragment extends Fragment {
         pbWorkout.setProgress(minWorkout*100/minTotals);
         pbCycling.setProgress(minCycling*100/minTotals);
         pbRunning.setProgress(minRunning*100/minTotals);
+
+
+        rbPeso.setChecked(true);
+        rbSemanal.setChecked(true);
+
+        rbSemanal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbSemanal.setChecked(true);
+                rbMensual.setChecked(false);
+            }
+        });
+
+        rbMensual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbSemanal.setChecked(false);
+                rbMensual.setChecked(true);
+            }
+        });
+
+        rbPeso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbPeso.setChecked(true);
+                rbCalorias.setChecked(false);
+                rbPasos.setChecked(false);
+            }
+        });
+
+        rbCalorias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbPeso.setChecked(false);
+                rbCalorias.setChecked(true);
+                rbPasos.setChecked(false);
+            }
+        });
+
+        rbPasos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rbPeso.setChecked(false);
+                rbCalorias.setChecked(false);
+                rbPasos.setChecked(true);
+            }
+        });
+
+
+        chart = view.findViewById(R.id.grafic);
+        ArrayList<Entry> valors = new ArrayList<>();
+
+        valors.add(new Entry(0, 2f));
+        valors.add(new Entry(1, 3f));
+        valors.add(new Entry(2, 2f));
+        valors.add(new Entry(3, 5f));
+        valors.add(new Entry(4, 6f));
+        valors.add(new Entry(5, 4f));
+
+        LineDataSet set1 = new LineDataSet(valors, "Set 1");
+        set1.setFillAlpha(110);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+        chart.setData(data);
 
         return view;
     }
