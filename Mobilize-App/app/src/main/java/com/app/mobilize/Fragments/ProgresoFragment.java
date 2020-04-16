76bfc821079054d882712e7721373ed051b041a0
AlexBrugal <60064808+AlexBrugal@users.noreply.github.com>
2020-04-16 20:25:59 +0200

    millores a la grafica seguiment

--- Mobilize-App/app/src/main/java/com/app/mobilize/Fragments/ProgresoFragment.java
+++ Mobilize-App/app/src/main/java/com/app/mobilize/Fragments/ProgresoFragment.java
@@ -21,6 +21,8 @@ import com.app.mobilize.activitatsUser;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -99,31 +101,46 @@ public class ProgresoFragment extends Fragment {
         rbPeso.setChecked(true);
         rbSemanal.setChecked(true);
 
+        //https://github.com/PhilJay/MPAndroidChart/wiki/The-Axis
+        YAxis left = chart.getAxisLeft();
+        left.setDrawLabels(false); // no axis labels
+        //left.setDrawAxisLine(false); // no axis line
+        left.setDrawGridLines(false); // no grid lines
+        chart.getAxisRight().setEnabled(false); // no right axis
+
         final ArrayList<Entry> valors_mensuals = new ArrayList<>();
         //valors per definicio de Pes i Setmanal
-        valors_mensuals.add(new Entry(0, 5f));
-        valors_mensuals.add(new Entry(1, 3f));
-        valors_mensuals.add(new Entry(2, 4f));
-        valors_mensuals.add(new Entry(3, 6f));
-        final LineDataSet setM = new LineDataSet(valors_mensuals, null);
+        valors_mensuals.add(new Entry(0, 78f));
+        valors_mensuals.add(new Entry(1, 76.5f));
+        valors_mensuals.add(new Entry(2, 77f));
+        valors_mensuals.add(new Entry(3, 74f));
+        final LineDataSet setM = new LineDataSet(valors_mensuals, "Mensual");
 
         final ArrayList<Entry> valors_setmanals = new ArrayList<>();
         //valors per definicio de Pes i Setmanal
-        valors_setmanals.add(new Entry(0, 3f));
-        valors_setmanals.add(new Entry(1, 4f));
-        valors_setmanals.add(new Entry(2, 1f));
-        valors_setmanals.add(new Entry(3, 5f));
-        valors_setmanals.add(new Entry(4, 3f));
-        valors_setmanals.add(new Entry(5, 2f));
-        valors_setmanals.add(new Entry(6, 2f));
-        final LineDataSet setS = new LineDataSet(valors_setmanals, null);
-        DrawGraph(setS);
+        valors_setmanals.add(new Entry(0, 77f));
+        valors_setmanals.add(new Entry(1, 77.9f));
+        valors_setmanals.add(new Entry(2, 77.4f));
+        valors_setmanals.add(new Entry(3, 76.8f));
+        valors_setmanals.add(new Entry(4, 77.2f));
+        valors_setmanals.add(new Entry(5, 76.5f));
+        valors_setmanals.add(new Entry(6, 76.4f));
+        final LineDataSet setS = new LineDataSet(valors_setmanals, "Semanal");
+        DrawGraph(setS, "peso", "semanal");
 
         rbSemanal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(rbMensual.isChecked()){
-                    DrawGraph(setS);
+                    if(rbPeso.isChecked()){
+                        DrawGraph(setS, "peso", "semanal");
+                    }
+                    if(rbCalorias.isChecked()){
+                        DrawGraph(setS, "calorias", "semanal");
+                    }
+                    if(rbPasos.isChecked()){
+                        DrawGraph(setS, "pasos", "semanal");
+                    }
                     chart.notifyDataSetChanged();
                     chart.invalidate();
                 }
@@ -136,7 +153,15 @@ public class ProgresoFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 if(rbSemanal.isChecked()){
-                    DrawGraph(setM);
+                    if(rbPeso.isChecked()){
+                        DrawGraph(setM, "peso", "mensual");
+                    }
+                    if(rbCalorias.isChecked()){
+                        DrawGraph(setM, "calorias", "mensual");
+                    }
+                    if(rbPasos.isChecked()){
+                        DrawGraph(setM, "pasos", "mensual");
+                    }
                     chart.notifyDataSetChanged();
                     chart.invalidate();
                 }
@@ -148,6 +173,16 @@ public class ProgresoFragment extends Fragment {
         rbPeso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(rbSemanal.isChecked()){
+                    DrawGraph(setS, "peso", "semanal");
+                }
+                if(rbMensual.isChecked()){
+                    DrawGraph(setM, "peso", "mensual");
+
+                }
+                chart.notifyDataSetChanged();
+                chart.invalidate();
+
                 rbPeso.setChecked(true);
                 rbCalorias.setChecked(false);
                 rbPasos.setChecked(false);
@@ -157,6 +192,15 @@ public class ProgresoFragment extends Fragment {
         rbCalorias.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(rbSemanal.isChecked()){
+                    DrawGraph(setS, "calorias", "semanal");
+                }
+                if(rbMensual.isChecked()){
+                    DrawGraph(setM, "calorias", "mensual");
+                }
+                chart.notifyDataSetChanged();
+                chart.invalidate();
+
                 rbPeso.setChecked(false);
                 rbCalorias.setChecked(true);
                 rbPasos.setChecked(false);
@@ -166,6 +210,15 @@ public class ProgresoFragment extends Fragment {
         rbPasos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(rbSemanal.isChecked()){
+                    DrawGraph(setS, "pasos", "semanal");
+                }
+                if(rbMensual.isChecked()){
+                    DrawGraph(setM, "pasos", "mensual");
+                }
+                chart.notifyDataSetChanged();
+                chart.invalidate();
+
                 rbPeso.setChecked(false);
                 rbCalorias.setChecked(false);
                 rbPasos.setChecked(true);
@@ -181,21 +234,32 @@ public class ProgresoFragment extends Fragment {
         startActivity(sigIntent);
     }
 
-    public void DrawGraph(LineDataSet set1){
+    public void DrawGraph(LineDataSet set1, String tipusdades, String temps){
 
         set1.setFillAlpha(110);
         set1.setLineWidth(2f);
-        set1.setColor(Color.parseColor("#00690a"));
+        set1.setColor(Color.parseColor("#50E33F"));
         set1.setValueTextColor(Color.BLACK);
         set1.setValueTextSize(6f);
+        set1.setDrawHorizontalHighlightIndicator(false);
 
         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
         dataSets.add(set1);
 
         Description desc = new Description();
-        desc.setText("Peso - KG");
-        desc.setTextSize(9f);
+        if(tipusdades == "peso"){
+            desc.setText("Peso - KG");
+        }
+        else  if(tipusdades == "calorias"){
+            desc.setText("Cal - Kcal");
+        }
+        else  if(tipusdades == "pasos"){
+            desc.setText("Pasos - m");
+        }
+        desc.setTextSize(12f);
+
         LineData data = new LineData(dataSets);
+        data.setValueTextSize(11f);
         chart.setData(data);
         chart.setDescription(desc);
         chart.setBorderColor(Color.GREEN);
